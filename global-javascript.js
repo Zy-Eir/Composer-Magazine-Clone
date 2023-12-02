572fcb8959848e9a94db16777992317197b17dbe
Zy-Eir <zyeiradam@gmail.com>
2023-12-02 12:39:54 -0500

    Added a dark-mode throughout the site. Decluttered nav-menu a little bit.

--- global-javascript.js
+++ global-javascript.js
@@ -1,22 +1,78 @@
-const hamButton = document.getElementById("hamburger-button");
+const menuButton = document.querySelector(".menu-button");
 const exitButton = document.getElementById("exit-button");
-const navSection = document.getElementById("nav-dropdown-section");
+const menuContainer = document.getElementById("menu-container");
+const menuContainerOpen = document.querySelector(".menu-container-open");
+const menuContainerClose = document.querySelector(".menu-container-close");
+const menuBackdrop = document.getElementById("menu-backdrop");
 
 
 
 
-hamButton.addEventListener('click', () => {
-    if (navSection.style.display ===
-        'none') {
-            navSection.style.display = 'grid'
-        } else {
-            navSection.style.display = 'none'
-        } 
-});
+const openMenu = function () {
+  menuContainer.classList.remove("menu-container-close");
+  menuContainer.classList.add("menu-container-open");
+  menuBackdrop.classList.add("menu-backdrop");
+  body.classList.add("disable-body-scroll");
+};
+menuButton.addEventListener("click", openMenu);
 
-exitButton.addEventListener('click', () => {
-    if (navSection.style.display ===
-        'grid') {
-            navSection.style.display = 'none'
-        }
-});
+const closeMenu = function () {
+    menuContainer.classList.add("menu-container-close");
+    menuContainer.classList.remove("menu-container-open");
+    menuBackdrop.classList.remove("menu-backdrop");
+    body.classList.remove("disable-body-scroll");
+  };
+  exitButton.addEventListener("click", closeMenu);
+
+  let darkMode = localStorage.getItem("darkmode");
+  const body = document.getElementById("body");
+  const p = document.getElementById("p");
+  const h1 = document.getElementById("h1");
+  const modeToggle = document.querySelector(".mode-toggle");
+  const toggleButton = document.getElementById("toggle-button");
+
+
+
+
+  const enableDarkMode = function () {
+        body.classList.add("dark-mode");
+        body.classList.add("dark-mode-text");
+      
+        toggleButton.classList.add("toggle-button-dark");
+        
+        localStorage.setItem('darkmode', 'enabled');
+        
+    };
+  
+
+
+
+  const disableDarkMode = function () {
+    body.classList.remove("dark-mode");
+    body.classList.remove("dark-mode-text");
+  
+    toggleButton.classList.remove("toggle-button-dark");
+    
+    localStorage.setItem('darkmode', 'disabled');
+    
+};
+
+  if (darkMode === "enabled") { //checks if dar
+      enableDarkMode();
+  }
+
+ modeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkmode");
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    console.log(darkMode);
+  } else {
+    disableDarkMode();
+    console.log(darkMode);
+  }
+ });
+
+
+
+
+
